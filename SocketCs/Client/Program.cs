774e39940e7986d791ad762f7dc4b69014eac585
HYEONHUYOON <101074004+HYEONHUYOON@users.noreply.github.com>
2023-05-28 23:19:58 +0900

    버그 수정 완

---
+++ SocketCs/Client/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client;
+            CLIUI cli = new CLIUI();
+            string IP = null;
+            string MSG = null;
+
+            bool connectStatus = false;
+
+            int inputNum = 0;
+
+            while (true)
+            {
+                client = new Client();
+                // 명령행 인수가 있으면 IP 주소로 사용
+                if (args.Length > 0) client.SERVERIP = args[0];
+
+                //접속한적 있을 때
+                if (connectStatus)
+                {
+                    inputNum = cli.QREpair();
+                    if (inputNum == 1)
+                    {
+                        MSG = cli.QMSG();
+                    }
+                    else if (inputNum == 2)
+                    {
+                        IP = cli.QIP();
+                    }
+                }
+                if (!connectStatus || inputNum == 0)
+                {
+                    Console.WriteLine("0 => 프로그램 종료");
+                    IP = cli.QIP();
+                    if (IP == "0")
+                    {
+                        break;
+                    }
+                    MSG = cli.QMSG();
+                }
+
+                connectStatus = client.Connect(IP, MSG);
+
+                Console.WriteLine("Enter 입력시 접속 종료");
+                if (connectStatus)
+                {
+                    while (true)
+                    {
+                        bool isClose = client.Write();
+                        //Enter시 접속 종료
+                        if (!isClose)
+                        {
+                            client.Close();
+                            break;
+                        }
+                        client.Read();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("접속 거부");
+                }
+            }
+            client.Close();
+        }
+    }
+}
---
+++ SocketCs/Server/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+//using Internal;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+
+            JsonManager json = new JsonManager();
+            json.LoadJson();
+            List<string> IpList = json.GetIP();
+
+            server.Bind();
+            server.Listen();
+            while (true)
+            {
+                try
+                {
+                    server.Accept(IpList);
+                    while (true)
+                    {
+                        server.Read();
+                        //아무것도 오지 않으면 => 연결이 끊키면
+                        if (server.retval == 0) break;
+                        server.Write();
+                        server.Send();
+                    }
+                    server.ClientSockClose();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    break;
+                }
+            }
+
+            //server.ServerClose();
+        }
+    }
+}
